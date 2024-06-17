9b82ed5ab9446daa88b0078b9c6cf43fe9a7de15
do-yoongyo2 <do.yoongyo2@gmail.com>
2024-06-17 09:35:53 +0900

    design: 튜토리얼 하단 설명 UI 너비 조정

--- src/components/CharacterExplainVer2.tsx
+++ src/components/CharacterExplainVer2.tsx
@@ -110,7 +110,7 @@ const CharacterExplainVer2 = ({
     >
       <div
         className="container mx-auto flex items-center justify-between"
-        style={{ maxWidth: "800px" }}
+        style={{ maxWidth: "70vw" }}
       >
         <div className="flex items-center">
           <img
