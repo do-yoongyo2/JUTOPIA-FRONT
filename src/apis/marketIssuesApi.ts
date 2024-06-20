e9cfa2cb5ec9590c52d874fb4627ecfadca0ae1b
do-yoongyo2 <do.yoongyo2@gmail.com>
2024-06-20 10:38:33 +0900

    feat: 신한투자증권 OpenAPI 주소 변경

--- src/apis/marketIssuesApi.ts
+++ src/apis/marketIssuesApi.ts
@@ -1,5 +1,8 @@
 import axios from "axios";
 
+// const URL = "52.78.236.23";
+const URL = "127.0.0.1";
+
 export const fetchMarketIssuesApi = () => {
-  return axios.get("http://52.78.236.23:3000/shinhan/market-issue");
+  return axios.get(`http://${URL}:3000/api/shinhan/market-issue`);
 };
