6d96ea3fa8980f5795b1f1a89435e8de7c8947b6
do-yoongyo2 <do.yoongyo2@gmail.com>
2024-06-21 10:00:09 +0900

    feat: ColumnBoard 조회 api로 변경

--- src/apis/marketIssuesApi.ts
+++ src/apis/marketIssuesApi.ts
@@ -4,5 +4,6 @@ const URL = "52.78.236.23";
 // const URL = "127.0.0.1";
 
 export const fetchMarketIssuesApi = () => {
-  return axios.get(`http://${URL}:3000/api/shinhan/market-issue`);
+  // return axios.get(`http://${URL}:3000/api/shinhan/market-issue`);
+  return axios.get(`http://${URL}:3000/api/columnboard/list`);
 };
